63b7f2bf064b04dbc2812a05ee96c4f481aa960d
sunbae.lee <sunbae.lee@riiid.co>
2022-06-09 15:06:23 +0900

    Initial Commit

---
+++ sunbae/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sunbae
+//
+//  Created by riiid on 2022/06/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sunbae/sunbaeApp.swift
@@ -0,0 +1,17 @@
+//
+//  sunbaeApp.swift
+//  sunbae
+//
+//  Created by riiid on 2022/06/09.
+//
+
+import SwiftUI
+
+@main
+struct sunbaeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
