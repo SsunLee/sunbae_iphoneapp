e204aca28bcfa3be81f8b49a7e32298f1349acb7
sunbae.lee <sunbae.lee@riiid.co>
2022-06-10 18:08:16 +0900

    add first

---
+++ sunbae/CirclsImage.swift
@@ -0,0 +1,20 @@
+//
+//  CirclsImage.swift
+//  sunbae
+//
+//  Created by riiid on 2022/06/09.
+//
+
+import SwiftUI
+
+struct CirclsImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CirclsImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CirclsImage()
+    }
+}
---
+++ sunbae/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  sunbae
+//
+//  Created by riiid on 2022/06/10.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
