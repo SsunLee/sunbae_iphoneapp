b3e2ea55afa41ef138d321bff34f154ac60a7dd2
sunbae.lee <sunbae.lee@riiid.co>
2022-06-10 16:58:03 +0900

    Initial Commit

---
+++ Shared/sunbae_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  sunbae_appApp.swift
+//  Shared
+//
+//  Created by riiid on 2022/06/10.
+//
+
+import SwiftUI
+
+@main
+struct sunbae_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
