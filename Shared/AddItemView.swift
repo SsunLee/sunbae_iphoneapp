e6807db3d88e30870964b808e7d69eb98c9b3d35
SsunLee <tnsqo1126@naver.com>
2022-06-14 02:45:09 +0900

    test save function

--- Shared/AddItemView.swift
+++ Shared/AddItemView.swift
@@ -14,42 +14,45 @@ struct AddItemView: View {
     @State var price: String = ""
     var body: some View {
         NavigationView {
-            VStack(alignment: .leading, spacing: 15) {
-                Spacer()
-                Label("title",systemImage: "pencil")
-                HStack {
-                    TextField("Enter your history", text: $title)
-                      .padding()
-                      .background(Color(uiColor: .secondarySystemBackground))
+            ScrollView() {
+                VStack(alignment: .leading, spacing: 15) {
+                    Spacer()
+                    Label("title",systemImage: "pencil")
+                    HStack {
+                        TextField("Enter your history", text: $title)
+                          .padding()
+                          .background(Color(uiColor: .secondarySystemBackground))
+                    }
+                    Spacer()
+                    Label("Member : ",systemImage: "person.3.sequence")
+                    HStack {
+                        TextField("Enter a members", text: $member)
+                          .padding()
+                          .background(Color(uiColor: .secondarySystemBackground))
+                    }
+                    Spacer()
+                    Label("Price : ",systemImage: "wonsign.circle")
+                    HStack {
+                        TextField("Enter a price", text: $price)
+                          .padding()
+                          .background(Color(uiColor: .secondarySystemBackground))
+                    }
+                    Spacer()
                 }
-                Spacer()
-                Label("Member : ",systemImage: "person.3.sequence")
-                HStack {
-                    TextField("Enter a members", text: $member)
-                      .padding()
-                      .background(Color(uiColor: .secondarySystemBackground))
+                .navigationTitle("자산 추가")
+                .toolbar {
+                    Button(action: {
+                        // if you button click
+                        AddItemRow()
+                        print("test")
+                    }) { // plus button
+                        Text("Save")
+                    }
+                    .accessibilityLabel("New")
                 }
-                Spacer()
-                Label("Price : ",systemImage: "wonsign.circle")
-                HStack {
-                    TextField("Enter a members", text: $member)
-                      .padding()
-                      .background(Color(uiColor: .secondarySystemBackground))
-                }
-                Spacer()
             }
-            .navigationTitle("자산 추가")
-            .toolbar {
-                Button(action: {
-                    // if you button click
-                    AddItemRow()
-                }) { // plus button
-                    Text("Save")
-                }
-                .accessibilityLabel("New")
+            .padding()
             }
-        }
-        .padding()
     }
     
     func AddItemRow() {
@@ -62,6 +65,9 @@ struct AddItemView: View {
                 title:title,
                 memger: setMember,
                 price: price)
+        )
+        historys.append(
+            CardInfo(title:"22ISAKAYA",memger: ["Sunbae","Geonsuk","Hosub"],price: "132,000")
         )
     }
     
